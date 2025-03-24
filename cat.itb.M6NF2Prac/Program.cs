5ee7ee7f8d3841065824f66e8b7415ccf1424b31
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-03-24 09:53:23 +0100

    Initial Commit - Created Project

---
+++ cat.itb.M6NF2Prac/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cat.itb.M6NF2Prac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
